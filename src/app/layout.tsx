259db765801724b3dcb33ca06cb2f565b2cfb8ea
ori raisfeld <raisfeldori@gmail.com>
2024-11-25 09:28:14 +0200

    text update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
       'Ori Raisfeld - junior software developer with more than 2 years of experience',
   },
   description:
-    'I"m Ori Raisfeld, a junior software developer with more than 2 years of experience. I build website and apps for clients and startups, from small local businesses to large corporations. I handle frontend, backend, and mobile development. I also design and manage the product roadmap. I love to build things that help people, and I hope you find value in my work.',
+    'fullstack dev with 2+ years of experience.',
   alternates: {
     types: {
       'application/rss+xml': `https://oriraisfeld.vercel.app/feed.xml`,
