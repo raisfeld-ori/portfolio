cbb6b8125431c65d1ca369812e870c05d76f7a9b
ori raisfeld <raisfeldori@gmail.com>
2024-12-22 12:25:18 +0200

    removed text

--- src/app/games/magic/page.tsx
+++ src/app/games/magic/page.tsx
@@ -4,7 +4,6 @@ import { MagicCard } from "@/components/Magic";
 
 export default function Magic(){
     return <Container className="mt-16 sm:mt-32">
-        <p>(NOTE: no tutorial yet. work in progress)</p>
         <MagicCard></MagicCard>
     </Container>
 }
